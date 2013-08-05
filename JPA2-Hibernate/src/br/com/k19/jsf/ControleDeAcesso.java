e524a3288e3125ea6b8de1c60de7ddc560375916
jrobertopr <jrobertopr@hotmail.com>
2013-08-05 15:55:20 -0300

    ControleDeAcesso

---
+++ JPA2-Hibernate/src/br/com/k19/jsf/ControleDeAcesso.java
@@ -0,0 +1,45 @@
+package br.com.k19.jsf;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+// registrando o filtro no WebContainer
+@WebFilter(servletNames={"Faces Servlet"})
+public class ControleDeAcesso implements Filter {
+	
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException {
+		
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpSession session = req.getSession();
+		
+		if(session.getAttribute("usuario") != null || req.getRequestURI().endsWith("login.xhtml")) {
+			chain.doFilter(request,  response);
+		} else {
+			HttpServletResponse res = (HttpServletResponse) response;
+			res.sendRedirect("login.xhtml");
+		}
+	}
+	
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		
+	}
+	
+	@Override
+	public void destroy() {
+		
+	}
+
+}

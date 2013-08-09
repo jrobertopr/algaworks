94c04a791580d75463706de99719097453ea0409
jrobertopr <jrobertopr@hotmail.com>
2013-08-09 15:29:27 -0300

    alteracao

--- JPA2-Hibernate/src/br/com/k19/filtro/JPAFilter.java
+++ JPA2-Hibernate/src/br/com/k19/filtro/JPAFilter.java
@@ -20,7 +20,7 @@ public class JPAFilter implements Filter {
 	
 	@Override
 	public void init(FilterConfig filterConfig) throws ServletException {
-		this.factory = Persistence.createEntityManagerFactory("K19_pu");
+		this.factory = Persistence.createEntityManagerFactory("K19-PU");
 	}
 	
 	@Override

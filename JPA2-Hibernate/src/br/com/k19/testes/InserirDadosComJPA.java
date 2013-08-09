52324fff9413033e17c4761ba6d99a41a430ad74
jrobertopr <jrobertopr@hotmail.com>
2013-08-09 15:23:43 -0300

    alteracao

--- JPA2-Hibernate/src/br/com/k19/testes/InserirDadosComJPA.java
+++ JPA2-Hibernate/src/br/com/k19/testes/InserirDadosComJPA.java
@@ -1,18 +1,16 @@
 package br.com.k19.testes;
 
-import java.util.Calendar;
-
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-import br.com.k19.mapeamento.Contato;
+import br.com.k19.entidade.Time;
 
 public class InserirDadosComJPA {
 	public static void main(String[] args) {
 
 		// manipulando entidade
-		EntityManagerFactory factory = Persistence.createEntityManagerFactory("livraria_pu");
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("K19-PU");
 
 		// criando entidade de gerenciamento
 		EntityManager manager = factory.createEntityManager();
@@ -21,14 +19,14 @@ public class InserirDadosComJPA {
 		manager.getTransaction().begin();
 		
 		// criando objetos
-		Contato novoContato = new Contato();
-		novoContato.setNome("Menescal Dias");
-		novoContato.setEmail("menescal@hotmail.com");
-		novoContato.setEndereco("Avenida do Coqueiro, 1145");
-		novoContato.setDataNascimento(Calendar.getInstance());
+		Time novoTime = new Time();
+		novoTime.setNome("Vasco");
+		novoTime.setTecnico("Caio Menezes");
+
+		// contato.setDataNascimento(new GregorianCalendar(1975, 11, 20));
 
 		// persistindo dados
-		manager.persist(novoContato);
+		manager.persist(novoTime);
 
 		// encerrando transação salvando as alterações
 		manager.getTransaction().commit();
